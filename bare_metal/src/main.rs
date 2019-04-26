1af7f28ef42d3edf32ee0ddc4063293e5641167b
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2019-04-26 09:51:03 -0400

    no std

--- bare_metal/src/main.rs
+++ bare_metal/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[no_mangle] 
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
