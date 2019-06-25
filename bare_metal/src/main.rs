6e3bac5a768de7b8756220f6e018bd72acc95406
Mathieu Lapointe <mathieu.lapointe3@mail.mcgill.ca>
2019-06-24 21:20:29 -0400

    successful exit after running test

--- bare_metal/src/main.rs
+++ bare_metal/src/main.rs
@@ -3,6 +3,7 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -13,6 +14,9 @@ static HELLO: &[u8] = b"Hello World!";
 pub extern "C" fn _start() -> ! {
 
     println!("Hello world{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -29,5 +33,31 @@ fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1,1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
