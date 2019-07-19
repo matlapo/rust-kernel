23175cf0c9aee4b7224bc4ebebafa5751e73a7ad
Mathieu Lapointe <matlapo@matlapo-mbp.local>
2019-07-19 15:38:03 -0400

    Restore

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use rust_kernel::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    rust_kernel::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_kernel::test_panic_handler(info)
+}
